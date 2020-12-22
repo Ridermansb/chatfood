e4add860d23ff5269bbe46c016e1f4cdae7a3b56
ridermansb <ridermansb@gmail.com>
2020-12-22 08:20:25 -0300

    Add githuh actions

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { getCLS, getFID, getLCP } from 'web-vitals';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import App from './App';
+
+getCLS(console.log);
+getFID(console.log);
+getLCP(console.log);
 
 const root = document.querySelector('#root');
 const renderApp = () => render(<App />, root);
